4acc57bccb0b5256ce66775b990d1418dab4d943
Oleksandr Cherepov <sashacherepov55@gmail.com>
2024-10-04 11:39:12 +0300

    Patched programme 1 in lab 2

--- lab2/programme/1/main.c
+++ lab2/programme/1/main.c
@@ -6,7 +6,7 @@ int main(void) {
     double P=1;
     double p=0;
     int ct = 0;
-    int j=1;
+    int j;
     int n;
     int i;
 
@@ -18,13 +18,13 @@ int main(void) {
     }
     else {
         for (i=1; i<=n; i++) {
-            while (j<=i) {
+            for (j=1; j<=i; j++) {
                 p+=j+sin(j);
-                j+=1;
                 ct+=6;
             }
             P*=((i*i)+1)/p;
-            ct+=7;
+            p=0;
+            ct+=8;
         }
         
         printf("Result: %.7lf\n", P);
