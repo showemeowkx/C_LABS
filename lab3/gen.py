c211b2b501348fdb05ab1becba769bfb89397d0c
Oleksandr Cherepov <sashacherepov55@gmail.com>
2024-10-18 14:15:26 +0300

    Lab3 done

--- lab3/gen.py
+++ lab3/gen.py
@@ -1,10 +1,10 @@
 from random import randint
 
 res=[]
-for i in range(0, 7):
+for i in range(0, 8):
     new_row=[]
     i+=1
-    for j in range(0, 7):
+    for j in range(0, 8):
         new_row.append(randint(-10,10))
         j+=1
     res.append(new_row)
--- lab3/programme/main.c
+++ lab3/programme/main.c
@@ -1,104 +1,71 @@
 #include <stdio.h>
 
-void linearSearch(double inputMatrix[7][7]){
-    int i;
-    int j;
-    int num1ID;
-    int num2ID;
-    double num1;
-    double num2;
-    double mainDiagonale[7]={};
+int main(void){
+    int i, j, n;
+    int num1ID, num2ID;
+    double num1=0;
+    double num2=0;
 
-    for (i=0; i<7; i++){
-        if (inputMatrix[i][i]>0) {
-            num1=inputMatrix[i][i];
-            num1ID=i;
-            break;
-        }
-    }    
-    for (i=6; i>=0; i--){
-        if (inputMatrix[i][i]<0){
-            num2=inputMatrix[i][i];
-            num2ID=i;
-            break;
-        }
-    }
+    printf("Welcome! Enter the size of your matrix: ");
+    scanf("%d", &n);
 
-    if (num1&&num2) {
-        inputMatrix[num1ID][num1ID]=num2;
-        inputMatrix[num2ID][num2ID]=num1;
-        printf("\nOutput matrix\n----------------------------------------------");
-        for (i=0; i<7; i++){
-            printf("\n");
-            for(j=0;j<7;j++){
-                printf("%.2lf ", inputMatrix[i][j]);
+    double matrix[n][n];
+
+    if (n>=7&&n<=10){
+        for(i=0; i<n; i++){
+            for(j=0; j<n ; j++){
+                printf("Element [%d][%d]: ", i+1, j+1);
+                scanf("%lf", &matrix[i][j]);
             }
         }
-        printf("\n----------------------------------------------");
+
+        printf("\nInput matrix:\n");
+        for (i=0; i<=n-1; i++){
+            for(j=0;j<=n-1;j++){
+                printf("%.2lf\t", matrix[i][j]);
+            }
+        printf("\n");
+        }
+
+        // double matrix[7][7]={{1,1,1,1,1,1,1},
+        //                     {1,1,1,1,1,1,1},
+        //                     {1,1,1,1,1,1,1},
+        //                     {1,1,1,1,1,1,1},
+        //                     {1,1,1,1,1,1,1},
+        //                     {1,1,1,1,1,1,1},
+        //                     {1,1,1,1,1,1,1}};
+        
+        for (i=0; i<=n-1; i++){
+            if (matrix[i][i]>0){
+                num1=matrix[i][i];
+                num1ID=i;
+                break;
+            }
+        }    
+
+        for (i=n-1; i>=0; i--){
+            if (matrix[i][i]<0){
+                num2=matrix[i][i];
+                num2ID=i;
+                break;
+            }
+        }
+
+        if (num1!=0&&num2!=0) {
+            matrix[num1ID][num1ID]=num2;
+            matrix[num2ID][num2ID]=num1;
+            printf("\nOutput matrix:\n");
+            for (i=0; i<=n-1; i++){
+                for(j=0;j<=n-1;j++){
+                    printf("%.2lf\t", matrix[i][j]);
+                }
+            printf("\n");
+            }
+        }
+        else printf("\nError! Not found!");
+        
     }
-    else printf("error");
-
-    // for (i=0; i<7; i++){
-    //     // printf("i: %d\n", i);
-    //     // printf("matrix element: %lf\n", inputMatrix[i][i]);
-    //     mainDiagonale[i]=inputMatrix[i][i];
-    // }
-
-    // for (i=0; i<7; i++){
-    //     if(mainDiagonale[i]>0){
-    //         num1[0]=i;
-    //         num1[1]=mainDiagonale[i];
-    //         break;
-    //     }
-    // }
-
-    // for (i=6; i>=0; i--){
-    //     if(mainDiagonale[i]<0) {
-    //         num2[0]=i;
-    //         num2[1]=mainDiagonale[i];
-    //         break;
-    //     }
-    // }
-
-    // if (num1[0]&&num1[1]&&num2[0]&&num2[1]) {
-    //     mainDiagonale[num1[0]]=num2[1];
-    //     mainDiagonale[num2[0]]=num1[1];
-    // }
-
-    
-    // // printf("num1: %.2lf\nnum2:%.2lf\n", num1[1], num2[1]);
-}
-
-int main(void){
-    double matrix1[7][7] = { {-1, 3, 4, -2, -1, 1, 6}, 
-                        {-1, 0, 9, 1, -3, -6, -8}, 
-                        {10, -10, 0, -4, -6, 10, 9},
-                        {-9, 1, 10, 1, 6, -4, -9},
-                        {10, -6, 4, 4, -5, -1, 1},
-                        {-2, 10, -1, 2, 6, -8, -2},
-                        {1, 7, 2, -1, -6, 0, -4}};
-
-    double matrix2[7][7] = { {-6, 4, 5, 2, -8, -7, -10}, 
-                        {5, -4, 5, 2, 3, -3, -5}, 
-                        {4, 5, -2, -7, 2, 4, -8},
-                        {7, 4, 7, 5, 3, -10, 3},
-                        {-8, -9, 3, 8, 6, 4, 7},
-                        {4, -1, 3, 8, -6, 10, -3},
-                        {-4, -1, 8, -7, -6, 0, -2}};
-
-    double matrix3[7][7] = { {-9, -3, 5, -4, 0, 1, -1}, 
-                        {-10, -10, 5, 2, -1, -2, 1}, 
-                        {-8, -7, 4, 8, -6, 9, -9},
-                        {8, -4, -1, -7, -9, 3, 10},
-                        {0, 7, -9, 1, -1, 1, 5},
-                        {-7, -6, 5, 3, 7, 1, 5},
-                        {10, -6, 3, 10, 3, 8, 4}};
-
-    linearSearch(matrix1);
-
-    linearSearch(matrix2);
-
-    linearSearch(matrix3);
+    else printf("Error! The size of matrix should be between or equal to 7 and 10.");
 
     return 0;
 }
